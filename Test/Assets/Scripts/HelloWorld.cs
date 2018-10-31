9fe37aa57bb95d0fa85a581a7ea163b6326ebdbc
Vasilis-Stratoglou <vasileios.stratoglou@byon8.com>
2018-10-31 10:49:10 +0100

    Add project

---
+++ Test/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HelloWorld : MonoBehaviour {
+
+    public InputField nameField;
+    public GameObject popup;
+    public Text Toast;
+
+    // Use this for initialization
+    void Start() {
+        hidePopupPanel();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void showPopupPanel() {
+        popup.SetActive(true);
+    }
+
+    public void hidePopupPanel()
+    {
+        popup.SetActive(false);
+    }
+
+    public void say() {
+        Toast.text = "Hello " + nameField.text;
+        showPopupPanel();
+    }
+}
